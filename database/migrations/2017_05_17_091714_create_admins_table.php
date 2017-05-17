84ae6ce0de21932e38c9f005e31ef95242723dbf
Rahadian Adinugroho <rahadian.adinugroho@live.com>
2017-05-17 09:41:14 +0700

    add migration for admins table

---
+++ database/migrations/2017_05_17_091714_create_admins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email', 100)->unique();
+            $table->string('password', 128);
+            $table->string('name', 64);
+            $table->integer('role')->unsigned();
+            $table->string('api_token', 64);
+            $table->timestamps();
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
