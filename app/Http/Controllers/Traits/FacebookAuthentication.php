dded76d77e87143fb418ca4d2472d27a36183d67
Rahadian Adinugroho <rahadian.adinugroho@live.com>
2017-05-18 13:43:11 +0700

    update trait logic

--- app/Http/Controllers/Traits/FacebookAuthentication.php
+++ app/Http/Controllers/Traits/FacebookAuthentication.php
@@ -26,57 +26,16 @@ trait FacebookAuthentication
     $fbid   = '';
     if (!empty($result->data) && $result->data->is_valid) {
       $fbid = $result->data->user_id;
-    }
-    if ($user = User::where('fb_id', '=', $fbid)->first()) {
-        return $user;
+        if ($user = User::where('fb_id', '=', $fbid)->first()) {
+            return $user;
+        }
     }
     return;
   }
 
   /**
-   * Get data from facebook using access token.
-   * @param  [String] $user_access_token [access token]
-   * @param  [String] $var_name          [name of field to get from Facebook's server]
-   * @return [String]                    [Result data on success, else boolean FALSE on failure.]
-   */
-  public function getFbData($user_access_token, $var_name)
-  {
-    $url = "https://graph.facebook.com/me?fields=".$var_name."&access_token=".$user_access_token;
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, $url);
-
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // our server
-
-    // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // facebook's server
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    $resultString = curl_exec($ch);
-
-    if (curl_errno($ch))
-    {
-      echo 'error: ' . curl_error($ch);
-      curl_close($ch);
-      return FALSE;
-    }
-    else
-    {
-      curl_close($ch);
-      $result = json_decode($resultString);
-      if(empty($result->error))
-      {
-        return $result;
-      }
-      else
-      {
-        return FALSE;
-      }
-    }
-  }
-
-  /**
-   * Utility function to be used by fb_auth function
+   * Utility function to be used by fb_auth function.
+   * 
    * @param  [String] $user_access_token [access token to authenticate]
    * @return [JSON Object]               [raw result from Facebook's Graph API]
    */
