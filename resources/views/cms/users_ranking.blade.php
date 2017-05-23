2e488a7c03fdcb4a05af1afd9b5d86831f78d9bd
Rahadian Adinugroho <rahadian.adinugroho@live.com>
2017-05-23 10:44:17 +0700

    rename users ranking view title

--- resources/views/cms/users_ranking.blade.php
+++ resources/views/cms/users_ranking.blade.php
@@ -33,7 +33,7 @@
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_title">
-            <h2>Reported users</h2>
+            <h2>Users ranking</h2>
             <ul class="nav navbar-right panel_toolbox">
               <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
               </li>
