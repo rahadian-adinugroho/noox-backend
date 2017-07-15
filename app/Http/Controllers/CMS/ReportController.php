1f7d3304df5d1adaa7335a0037e9dff44bbc1556
Rahadian Adinugroho <rahadian.adinugroho@live.com>
2017-07-15 23:36:53 +0700

    add report details

--- app/Http/Controllers/CMS/ReportController.php
+++ app/Http/Controllers/CMS/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace Noox\Http\Controllers\CMS;
 
+use Noox\Models\Report;
 use Illuminate\Http\Request;
 use Noox\Http\Controllers\Controller;
 
@@ -13,12 +14,26 @@ class ReportController extends Controller
     }
 
     /**
-     * Return the all news
-     * 
+     * View reports table.
+     *
      * @return Illuminate\Http\Response
      */
     public function index()
     {
         return view('cms.reports');
+    }
+
+    /**
+     * View report details.
+     *
+     * @param int $id
+     * @return Illuminate\Http\Response
+     */
+    public function view($id)
+    {
+        if (! $data = Report::with(['reporter', 'status'])->find($id)) {
+            abort(404);
+        }
+        return view('cms.report_details', compact('data'));
     }
 }
---
+++ resources/views/cms/report_details.blade.php
@@ -0,0 +1,194 @@
+@extends('layouts.admin')
+
+@section('pagespecificmetas')
+<meta name="context-id" content="{{ $data->id }}">
+<meta name="context-type" content="{{ $data->reportable_type }}">
+@stop
+
+@section('pagespecificstyles')
+
+@stop
+
+@section('content')
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Report Details</h3>
+      </div>
+
+      <div class="title_right">
+        <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="button">Go!</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Data</h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+              </li>
+              <li><a class="close-link"><i class="fa fa-close"></i></a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <br>
+            <form id="data-form" data-parsley-validate="" class="form-horizontal form-label-left" novalidate="">
+              <div class="form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="reporter-name">Reporter
+                </label>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  <input type="text" id="reporter-name" name="reporter-name" value="{{ $data->reporter->name }}" required="required" minlength="3" class="form-control col-md-7 col-xs-12" readonly>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="date">Date
+                </label>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  <input type="date" id="date" name="date" value="{{ $data->created_at->format('Y-m-d') }}" required="required" class="form-control col-md-7 col-xs-12" readonly>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12">Status</label>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  <div id="status" class="btn-group" data-toggle="buttons">
+                    <label class="btn btn-danger {{ ($data->status->name == 'open') ? 'active' : '' }}" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                      <input type="radio" name="status" value="open" data-parsley-multiple="status" {{ ($data->status->name == 'open') ? 'checked' : '' }} disabled> &nbsp; Open &nbsp;
+                    </label>
+                    <label class="btn btn-warning {{ ($data->status->name == 'investigating') ? 'active' : '' }}" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                      <input type="radio" name="status" value="investigating" data-parsley-multiple="status" {{ ($data->status->name == 'investigating') ? 'checked' : '' }} disabled> &nbsp; Investigating &nbsp;
+                    </label>
+                    @if ($data->reportable_type === 'news')
+                    <label class="btn btn-success {{ ($data->status->name == 'approved') ? 'active' : '' }}" data-toggle="tooltip" data-placement="top" data-original-title="Delete the news instead!" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                      <input type="radio" name="status" value="approved" data-parsley-multiple="status" {{ ($data->status->name == 'approved') ? 'checked' : '' }} disabled> Approved
+                    </label>
+                    @else
+                    <label class="btn btn-default {{ ($data->status->name == 'closed') ? 'active' : '' }}" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                      <input type="radio" name="status" value="closed" data-parsley-multiple="status" {{ ($data->status->name == 'closed') ? 'checked' : '' }} disabled> &nbsp; Closed &nbsp;
+                    </label>
+                    <label class="btn btn-success {{ ($data->status->name == 'solved') ? 'active' : '' }}" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                      <input type="radio" name="status" value="solved" data-parsley-multiple="status" {{ ($data->status->name == 'solved') ? 'checked' : '' }} disabled> &nbsp; Solved &nbsp;
+                    </label>
+                    @endif
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12">Content
+                </label>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  <textarea id="content" name="content" class="form-control col-md-7 col-xs-12" required="required" disabled>
+                  {{ $data->content }}
+                  </textarea>
+                </div>
+              </div>
+              <div class="ln_solid"></div>
+              <div class="form-group">
+                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                  @if ($data->reportable_type == 'news')
+                  <a href="{{ route('cms.news.details', $data->reportable_id) }}"><button type="button" id="edit-button" class="btn btn-primary">View Reported Item</button></a>
+                  @elseif ($data->reportable_type == 'user')
+                  <a href="{{ route('cms.user.profile', $data->reportable_id) }}"><button type="button" id="edit-button" class="btn btn-primary">View Reported Item</button></a>
+                  @else 
+                  <a href="{{ route('cms.news.comment.details', $data->reportable_id) }}"><button type="button" id="edit-button" class="btn btn-primary">View Reported Item</button></a>
+                  @endif
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+@section('pagespecificscripts')
+<script type="text/javascript">
+  let targetType = null;
+  let targetId = null;
+  let currentStatus = null;
+
+  $( document ).ready(function (e) {
+    targetType = $('meta[name="context-type"]').prop('content');
+    targetId   = getContextId();
+    currentStatus = $('#status input[checked]').val();
+
+    if (currentStatus === 'solved' || currentStatus === 'approved' || currentStatus === 'closed') {
+      if (currentStatus === 'closed') {
+        elements = $('#status label input[type="radio"]:not([value="closed"]):not([value="solved"]):not([value="approved"])');
+      } else {
+        elements = $('#status label input[type="radio"]:not([value="solved"]):not([value="approved"])');
+      }
+
+      elements.each(function (i, el){
+        $(el).attr('disabled', true);
+        $(el).parent().attr('disabled', true);
+      })
+    }
+  });
+
+  $('#status label').on('click', function (e){
+    let status = $(this).find('input').val();
+
+    if (currentStatus === 'solved' || currentStatus === 'approved' || (status === currentStatus)) {
+      return undefined;
+    }
+
+    if (currentStatus === 'investigating' && status === 'open') {
+      nopeAlert();
+      return undefined;
+    }
+    if (currentStatus === 'closed' && (status === 'open' || status === 'investigating')) {
+      nopeAlert();
+      return undefined;
+    }
+    if (currentStatus === 'solved' || currentStatus === 'approved') {
+      nopeAlert();
+      return undefined;
+    }
+
+    updateStatus(status);
+  });
+
+  function updateStatus(status, type=targetType) {
+    let availableStatus = ['open', 'investigating', 'closed', 'solved', 'approved'];
+
+    if (! availableStatus.includes(status)) {
+      console.error('Invalid status supplied.');
+      return undefined;
+    }
+    if (type === 'news' && status === 'solved') {
+      console.error('This status is unavailable for news.');
+      return undefined;
+    }
+
+    if (type === 'news' && status === 'approved') {
+      swalConfirm(function (c) {
+        axios.put(gCmsApiBase + '/report/' + targetId, {status: 'approved'})
+      }, 'Attention!', 'Deleting the news will set all reports for this particular news to approved. Delete the news instead of updating the report status!');
+    } else {
+      swalConfirm(function (c) {
+        axios.put(gCmsApiBase + '/report/' + targetId, {status: status})
+      }, 'Are you sure?', 'Once updated, the status cannot be reverted to previous status.', 'question');
+    }
+  }
+
+  function nopeAlert() {
+    swal('Warning!', 'You cannot update to this status!', 'error');
+  }
+</script>
+@stop
